30f676664d415110fa0d8bc9ab248631ad46628c
Joi Glifberg <joi.glifberg@gmail.com>
2022-03-31 13:48:33 +0200

    Setup Sendgrid subuser

--- commands/sendgrid.js
+++ commands/sendgrid.js
@@ -1,8 +1,10 @@
 const { getProjectName } = require("../lib/app-name")
 const { program } = require("commander")
-const { writeStep, writeInfo } = require("../lib/write")
+const { writeStep, writeInfo, writeSuccess } = require("../lib/write")
+const client = require("@sendgrid/client")
 const conf = require("../lib/conf")
-const fetch = require("node-fetch")
+const setup = require("../commands/setup")
+const passwordGenerator = require("generate-password")
 
 program
   .command("sendgrid")
@@ -10,36 +12,40 @@ program
   .action(setupSendgridAccount)
 
 async function setupSendgridAccount() {
+  writeStep("Setting up Sendgrid subuser")
+
   let projectName = await getProjectName()
-  let sendgrid = conf.get("sendgrid") || (await setup("sendgrid")).sendgrid
-  let { apiKey } = sendgrid
 
-  writeStep("Setup Sendgrid account")
-
-  let data = await request("/subusers", {
-    username: projectName,
-    email: `${projectName}@sendgrid.triggerfish.se`,
-    password: "",
-    ips: ["167.89.92.184"],
-  })
-
-  writeInfo(projectName)
-  writeInfo(apiKey)
-}
-
-async function request(endpoint, postData) {
   let { apiKey } = conf.get("sendgrid") || (await setup("sendgrid")).sendgrid
 
-  let response = await fetch("https://api.sendgrid.com/v3" + endpoint, {
-    method: "POST",
-    body: JSON.stringify(postData),
-    headers: {
-      Authorization: `Bearer ${apiKey}`,
-      "content-type": "application/json",
-    },
-  })
+  client.setApiKey(apiKey)
 
-  return response.json()
+  try {
+    let [, ips] = await client.request({
+      method: "GET",
+      url: "/v3/ips/assigned",
+    })
+
+    const ip = ips[0].ip
+
+    let [, body] = await client.request({
+      method: "POST",
+      url: "/v3/subusers",
+      body: {
+        username: projectName,
+        email: `${projectName}@sendgrid.triggerfish.se`,
+        password: passwordGenerator.generate({
+          numbers: true,
+          symbols: true,
+        }),
+        ips: [ip],
+      },
+    })
+
+    writeSuccess(`Sendgrid subuser created`)
+  } catch (e) {
+    console.error(e)
+  }
 }
 
 module.exports = setupSendgridAccount
