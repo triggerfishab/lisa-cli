fed74635a37748c8ce6ab1c403e7a38009d34d54
Joi Glifberg <joi.glifberg@gmail.com>
2022-03-31 13:27:54 +0200

    Add Sendgrid stuff WIP

--- commands/sendgrid.js
+++ commands/sendgrid.js
@@ -2,6 +2,7 @@ const { getProjectName } = require("../lib/app-name")
 const { program } = require("commander")
 const { writeStep, writeInfo } = require("../lib/write")
 const conf = require("../lib/conf")
+const fetch = require("node-fetch")
 
 program
   .command("sendgrid")
@@ -15,8 +16,30 @@ async function setupSendgridAccount() {
 
   writeStep("Setup Sendgrid account")
 
+  let data = await request("/subusers", {
+    username: projectName,
+    email: `${projectName}@sendgrid.triggerfish.se`,
+    password: "",
+    ips: ["167.89.92.184"],
+  })
+
   writeInfo(projectName)
   writeInfo(apiKey)
+}
+
+async function request(endpoint, postData) {
+  let { apiKey } = conf.get("sendgrid") || (await setup("sendgrid")).sendgrid
+
+  let response = await fetch("https://api.sendgrid.com/v3" + endpoint, {
+    method: "POST",
+    body: JSON.stringify(postData),
+    headers: {
+      Authorization: `Bearer ${apiKey}`,
+      "content-type": "application/json",
+    },
+  })
+
+  return response.json()
 }
 
 module.exports = setupSendgridAccount
