9108107ac0fc8a6f5f64168497b627615c92d707
Joi Glifberg <joi.glifberg@gmail.com>
2022-04-05 10:58:18 +0200

    Fix Kinsta yml format

--- help/kinsta.js
+++ help/kinsta.js
@@ -1,40 +1,40 @@
 function getKinstaHelpMessage() {
   return `\nTemplate file:
 
-  # kinsta.yml
+# kinsta.yml
 
-  sitename: api-lisa.se
-  staging:
-    canonical: staging-lisa-se.kinsta.cloud
+sitename: api-lisa.se
+staging:
+  canonical: staging-lisa-se.kinsta.cloud
 
-    db_name: lisa
-    db_password: lisa123
-    db_user: lisa
+  db_name: lisa
+  db_password: lisa123
+  db_user: lisa
 
-    project_root: /www/example_123/public
-    web_user: example
-    web_group: www-data
-    www_root: /www/example_123/public
+  project_root: /www/example_123/public
+  web_user: example
+  web_group: www-data
+  www_root: /www/example_123/public
 
-    ansible_host: 192.168.0.1
-    ansible_ssh_port: 12345
+  ansible_host: 192.168.0.1
+  ansible_ssh_port: 12345
 
-  production:
-    canonical: lisa-se.kinsta.cloud
-    redirects:
-      - www.lisa-se.kinsta.cloud
+production:
+  canonical: lisa-se.kinsta.cloud
+  redirects:
+    - www.lisa-se.kinsta.cloud
 
-    db_name: lisa
-    db_password: lisa123
-    db_user: lisa
+  db_name: lisa
+  db_password: lisa123
+  db_user: lisa
 
-    project_root: /www/example_123/public
-    web_user: example
-    web_group: www-data
-    www_root: /www/example_123/public
+  project_root: /www/example_123/public
+  web_user: example
+  web_group: www-data
+  www_root: /www/example_123/public
 
-    ansible_host: 192.168.0.1
-    ansible_ssh_port: 12345
+  ansible_host: 192.168.0.1
+  ansible_ssh_port: 12345
 `
 }
 
