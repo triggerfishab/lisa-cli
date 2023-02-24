705f87930c0915f96a1c784f46451c3833621a79
Johan Möller <moelleerjohan@gmail.com>
2023-02-24 16:46:40 +0100

    Add some default fields to admin component

--- templates/pageComponents/page-component.php
+++ templates/pageComponents/page-component.php
@@ -4,5 +4,23 @@ return [
     'key' => 'field_page_components_COMPONENTNAMESNAKECASE',
     'name' => 'COMPONENTNAMESNAKECASE',
     'label' => esc_html__('COMPONENTLABEL', 'lisa'),
-    'sub_fields' => []
+    'sub_fields' => [
+        [
+            'key' => 'field_page_components_COMPONENTNAMESNAKECASE_content_tab',
+            'type' => 'tab',
+            'label' => esc_html__('Content', 'lisa'),
+        ],
+        [
+            'key' => 'field_page_components_COMPONENTNAMESNAKECASE_settings_tab',
+            'type' => 'tab',
+            'label' => esc_html__('Settings', 'lisa'),
+        ],
+        [
+            'key' => 'field_page_components_content_COMPONENTNAMESNAKECASE_settings_name',
+            'type' => 'clone',
+            'clone' => [
+                'field_clone_component_settings_component_name',
+            ],
+        ],
+    ]
 ];
