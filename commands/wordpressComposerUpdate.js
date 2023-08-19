ce09019fb036c2b95919295527d6ce46c1a739b8
Christian Widlund <christian.widlund@gmail.com>
2023-08-19 15:20:49 +0200

    use oneliner when updating packages

--- commands/wordpressComposerUpdate.js
+++ commands/wordpressComposerUpdate.js
@@ -1,40 +1,46 @@
 import asyncExec from "../lib/exec.js"
 import fs from "fs"
-import { writeError, writeInfo, writeSuccess } from "../lib/write.js"
+import { writeError } from "../lib/write.js"
+import { versions } from "../lib/versions.js"
+import semver from "semver"
 
 export async function wpUpdate() {
-  try {
-    let composerValidateOutput = await asyncExec(
-      "composer validate --no-check-all --strict --no-interaction",
-      {
-        cwd: process.env.PWD,
-      },
-    )
+  const asyncExecOptions = {
+    stdio: "inherit",
+    cwd: process.env.PWD,
+  }
 
-    writeSuccess(composerValidateOutput.stderr.trim())
+  const parsedPhpVersion = semver.parse(versions.php, {})
+  const phpVersion = `>=${parsedPhpVersion.major}.${parsedPhpVersion.minor}`
+
+  try {
+    const composerValidateOutput = await asyncExec(
+      "composer validate --no-check-all --strict --no-interaction --ansi",
+      asyncExecOptions
+    )
+    console.log(composerValidateOutput.stdout)
 
     const composerJson = JSON.parse(
-      fs.readFileSync(process.env.PWD + "/composer.json", "utf8"),
+      fs.readFileSync(process.env.PWD + "/composer.json", "utf8")
     )
-    if (composerJson.require.php !== ">=8.1") {
-      composerJson.require.php = ">=8.1"
-      fs.writeFileSync(
-        process.env.PWD + "/composer.json",
-        JSON.stringify(composerJson, null, 2),
-      )
-    }
 
-    for (const pluginName in composerJson.require) {
-      if (pluginName !== "php") {
-        let composerRequireOutput = await asyncExec(
-          `composer require ${pluginName} --with-all-dependencies --no-interaction`,
-          {
-            cwd: process.env.PWD,
-          },
-        )
-        writeSuccess(composerRequireOutput.stderr.trim())
+    let requiredComposerPackages = ""
+    for (const [requiredComposerPackage] of Object.entries(
+      composerJson.require
+    )) {
+      if (requiredComposerPackage === "php") {
+        requiredComposerPackages += `${requiredComposerPackage}${phpVersion} `
+      }
+      if (requiredComposerPackage !== "php") {
+        requiredComposerPackages += `${requiredComposerPackage} `
       }
     }
+
+    const composerRequireOutput = await asyncExec(
+      `composer require ${requiredComposerPackages} --with-all-dependencies --no-interaction --no-progress --ansi`,
+      asyncExecOptions
+    )
+    console.log(composerRequireOutput.stderr.trim())
   } catch (err) {
     writeError(err)
     process.exit(1)
