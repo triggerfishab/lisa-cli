7a0b5089b062bf917278646d5914193f61abd5f8
Christian Widlund <christian.widlund@gmail.com>
2023-08-24 10:21:25 +0200

    commit todos

--- commands/wordpressComposerUpdate.js
+++ commands/wordpressComposerUpdate.js
@@ -10,23 +10,50 @@ export async function wpUpdate() {
     cwd: process.env.PWD,
   }
 
+  //  TODO: list major vesionsas as output at the end
+  //  TODO: add done yay message
+  //  TODO: add description to program
+  // TODO: loop through every packaage change
+  // TODO:  update dev dependencies
+  // TODO: * bonus check configured repositories for correct kinsta
+  // TODO: * bonus check configured repositories for correct triggerfish composer
+  //   "repositories": {
+  //     "0": {
+  //       "type": "composer",
+  //       "url": "https://wpackagist.org",
+  //       "only": [
+  //         "wpackagist-plugin/*",
+  //         "wpackagist-theme/*"
+  //       ]
+  //     },
+  //     "composer.triggerfish.cloud": {
+  //       "type": "composer",
+  //       "url": "https://composer.triggerfish.cloud/",
+  //       "only": [
+  //         "triggerfish/*",
+  //         "rocketgenius/gravity-forms",
+  //         "wpengine/advanced-custom-fields-pro"
+  //       ]
+  //     }
+  //   },
+
   const parsedPhpVersion = semver.parse(versions.php, {})
   const phpVersion = `>=${parsedPhpVersion.major}.${parsedPhpVersion.minor}`
 
   try {
     const composerValidateOutput = await asyncExec(
       "composer validate --no-check-all --strict --no-interaction --ansi",
-      asyncExecOptions
+      asyncExecOptions,
     )
     console.log(composerValidateOutput.stdout)
 
     const composerJson = JSON.parse(
-      fs.readFileSync(process.env.PWD + "/composer.json", "utf8")
+      fs.readFileSync(process.env.PWD + "/composer.json", "utf8"),
     )
 
     let requiredComposerPackages = ""
     for (const [requiredComposerPackage] of Object.entries(
-      composerJson.require
+      composerJson.require,
     )) {
       if (requiredComposerPackage === "php") {
         requiredComposerPackages += `"${requiredComposerPackage}:${phpVersion}" `
@@ -38,7 +65,7 @@ export async function wpUpdate() {
 
     const composerRequireOutput = await asyncExec(
       `composer require ${requiredComposerPackages} --with-all-dependencies --no-interaction --no-progress --ansi`,
-      asyncExecOptions
+      asyncExecOptions,
     )
     console.log(composerRequireOutput.stderr.trim())
   } catch (err) {
