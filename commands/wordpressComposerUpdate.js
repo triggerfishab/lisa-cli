4d7bc7356881e323390cbad92149def7dff0bff9
Christian Widlund <christian.widlund@gmail.com>
2024-04-11 11:58:23 +0200

    feat(wp update): ✨ fast wp update (#82)
    * set minimum-stability to dev
    minimum-stability (root-only)#
    This defines the default behavior for filtering packages by stability. This defaults to stable, so if you rely on a dev package, you should specify it in your file to avoid surprises.
    
    All versions of each package are checked for stability, and those that are less stable than the minimum-stability setting will be ignored when resolving your project dependencies. (Note that you can also specify stability requirements on a per-package basis using stability flags in the version constraints that you specify in a require block (see package links for more details).
    
    Available options (in order of stability) are dev, alpha, beta, RC, and stable.
    
    set prefer-stable true
    prefer-stable (root-only)#
    When this is enabled, Composer will prefer more stable packages over unstable ones when finding compatible stable packages is possible. If you require a dev version or only alphas are available for a package, those will still be selected granted that the minimum-stability allows for it.
    
    Use "prefer-stable": true to enable.
    
    skip ext- dependencies
    
    Note: It is important to list PHP extensions your project requires. Not all PHP installations are created equal: some may miss extensions you may consider as standard (such as ext-mysqli which is not installed by default in Fedora/CentOS minimal installation systems). Failure to list required PHP extensions may lead to a bad user experience: Composer will install your package without any errors but it will then fail at run-time. The composer show --platform command lists all PHP extensions available on your system. You may use it to help you compile the list of extensions you use and require. Alternatively you may use third party tools to analyze your project for the list of extensions used.
    
    * add --fast to wp update command
    
    * check if require-dev is defined or empty
    
    * always write composer json file
    
    * combines required packages and required-dev packages into one command into one line

--- commands/wordpressComposerUpdate.js
+++ commands/wordpressComposerUpdate.js
@@ -45,58 +45,114 @@ function setPreferredStability(preferStable) {
 }
 
 /**
+ * @param {object} composerJson
  * @param {string} phpVersion
+ * @param {object} asyncExecOptions
+ * @param {string} asyncExecOptions.stdio
+ * @param {string} asyncExecOptions.cwd
+ * @param {boolean} [fast=false]
  * @returns {Promise<void>}
  */
-async function updateRequirePackages(phpVersion) {
-  const composerJson = getComposerJson()
-
-  for (const requiredComposerPackage of Object.keys(composerJson.require)) {
-    let requireCommand = ""
-    if (requiredComposerPackage.startsWith("ext-")) {
-      continue
+async function updateRequirePackages(
+  composerJson,
+  phpVersion,
+  asyncExecOptions,
+  fast,
+) {
+  if (fast === true) {
+    let requiredComposerPackages = ""
+    for (const [requiredComposerPackage] of Object.entries(
+      composerJson.require,
+    )) {
+      if (requiredComposerPackage.startsWith("ext-")) {
+        continue
+      }
+      if (requiredComposerPackage === "php") {
+        requiredComposerPackages += `"${requiredComposerPackage}:${phpVersion}" `
+      }
+      if (requiredComposerPackage !== "php") {
+        requiredComposerPackages += `${requiredComposerPackage} `
+      }
     }
-    if (requiredComposerPackage === "php") {
-      requireCommand = `composer require "${requiredComposerPackage}:${phpVersion}" --with-all-dependencies --no-interaction  --ansi`
-    }
-    if (requiredComposerPackage !== "php") {
-      requireCommand = `composer require ${requiredComposerPackage} --with-all-dependencies --no-interaction --no-progress --ansi`
-    }
-    const requiredOutput = await asyncExec(
-      requireCommand,
-      getAsyncExecOptionsForPWD(),
-    )
+    const requireCommand = `composer require ${requiredComposerPackages} --with-all-dependencies --no-interaction --no-progress --ansi`
+    const requiredOutput = await asyncExec(requireCommand, asyncExecOptions)
     writeInfo(`running: ${requireCommand}`)
     console.log(requiredOutput.stdout || requiredOutput.stderr)
+  }
+  if (fast === false) {
+    for (const [requiredComposerPackage] of Object.entries(
+      composerJson.require,
+    )) {
+      let requireCommand = ""
+      if (requiredComposerPackage.startsWith("ext-")) {
+        continue
+      }
+      if (requiredComposerPackage === "php") {
+        requireCommand = `composer require "${requiredComposerPackage}:${phpVersion}" --with-all-dependencies --no-interaction  --ansi`
+      }
+      if (requiredComposerPackage !== "php") {
+        requireCommand = `composer require ${requiredComposerPackage} --with-all-dependencies --no-interaction --no-progress --ansi`
+      }
+      const requiredOutput = await asyncExec(requireCommand, asyncExecOptions)
+      writeInfo(`running: ${requireCommand}`)
+      console.log(requiredOutput.stdout || requiredOutput.stderr)
+    }
   }
 }
 
 /**
+ * @param {object} composerJson
+ * @param {object} asyncExecOptions
+ * @param {string} asyncExecOptions.stdio
+ * @param {string} asyncExecOptions.cwd
+ * @param {boolean} [fast=false]
  * @returns {Promise<void>}
  */
-async function updateRequireDevPackages() {
-  const composerJson = getComposerJson()
+async function updateRequireDevPackages(composerJson, asyncExecOptions, fast) {
+  if (
+    !Object.prototype.hasOwnProperty.call(composerJson, "require-dev") ||
+    Object.keys(composerJson["require-dev"]).length === 0
+  ) {
+    return
+  }
 
-  for (const requiredDevComposerPackage of Object.keys(
-    composerJson["require-dev"],
-  )) {
-    const requireCommand = `composer require ${requiredDevComposerPackage} --dev --with-all-dependencies --no-interaction --no-progress --ansi`
+  if (fast === true) {
+    const requiredDevComposerPackages = Object.keys(
+      composerJson["require-dev"],
+    ).join(" ")
+    const requireCommand = `composer require ${requiredDevComposerPackages} --dev --with-all-dependencies --no-interaction --no-progress --ansi`
     const requiredOutput = await asyncExec(
-      `composer require ${requiredDevComposerPackage} --dev --with-all-dependencies --no-interaction --no-progress --ansi`,
-      getAsyncExecOptionsForPWD(),
+      `composer require ${requiredDevComposerPackages} --dev --with-all-dependencies --no-interaction --no-progress --ansi`,
+      asyncExecOptions,
     )
     writeInfo(`running: ${requireCommand}`)
     console.log(requiredOutput.stdout || requiredOutput.stderr)
   }
+  if (fast === false) {
+    for (const [requiredDevComposerPackage] of Object.entries(
+      composerJson["require-dev"],
+    )) {
+      const requireCommand = `composer require ${requiredDevComposerPackage} --dev --with-all-dependencies --no-interaction --no-progress --ansi`
+      const requiredOutput = await asyncExec(
+        `composer require ${requiredDevComposerPackage} --dev --with-all-dependencies --no-interaction --no-progress --ansi`,
+        asyncExecOptions,
+      )
+      writeInfo(`running: ${requireCommand}`)
+      console.log(requiredOutput.stdout || requiredOutput.stderr)
+    }
+  }
 }
 
 /**
+ * @param {object} asyncExecOptions
+ * @param {string} asyncExecOptions.stdio
+ * @param {string} asyncExecOptions.cwd
  * @return {Promise<void>}
  */
-async function validateComposerFile() {
+async function validateComposerFile(asyncExecOptions) {
   const composerValidateOutput = await asyncExec(
     "composer validate --no-check-all --strict --no-interaction --ansi",
-    getAsyncExecOptionsForPWD(),
+    asyncExecOptions,
   )
   console.log(composerValidateOutput.stdout)
 }
@@ -109,30 +165,36 @@ function getComposerJson() {
 }
 
 /**
- * @return {{cwd: string, stdio: string}}
+ * @param {string} args
+ * @param {object} options
+ * @param {boolean} [options.fast=false]
+ * @returns {Promise<void>}
  */
-function getAsyncExecOptionsForPWD() {
-  return {
+export default async function wpUpdate(args, options) {
+  const fast = options.fast
+
+  const asyncExecOptions = {
     stdio: "inherit",
     cwd: process.env.PWD,
   }
-}
 
-/**
- * @returns {Promise<void>}
- */
-export default async function wpUpdate() {
   const parsedPhpVersion = semver.parse(versions.php, {})
   const phpVersion = `>=${parsedPhpVersion.major}.${parsedPhpVersion.minor}`
   const minimumStability = "dev"
   const preferStable = true
 
   try {
-    await validateComposerFile()
+    await validateComposerFile(asyncExecOptions)
+    const composerJson = getComposerJson()
     setMinimumStability(minimumStability)
     setPreferredStability(preferStable)
-    await updateRequirePackages(phpVersion)
-    await updateRequireDevPackages()
+    await updateRequirePackages(
+      composerJson,
+      phpVersion,
+      asyncExecOptions,
+      fast,
+    )
+    await updateRequireDevPackages(composerJson, asyncExecOptions, fast)
 
     writeWarning("Don't forget to Check your Major Updates.")
     writeSuccess("Composer update completed")
--- index.js
+++ index.js
@@ -119,12 +119,16 @@ async function initProgram() {
 
   program
     .command("wp update")
-    .description(
-      `Update WordPress and Composer dependencies.
-Make sure your standing in the folder where your composer.json file is located.
-    `,
+    .description("Update WordPress and Composer dependencies.")
+    .addHelpCommand(
+      "Make sure your standing in the folder where your composer.json file is located.",
     )
     .action(wpUpdate)
+    .option(
+      "-f, --fast",
+      "combines required packages and required-dev packages into one command into one line",
+      false,
+    )
 
   program
     .command("godaddy create")
