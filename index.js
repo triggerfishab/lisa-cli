2b3b62159fdfb4c727cc78b7c14713142104d965
Per Lisshamre <2478763+lissmeister@users.noreply.github.com>
2025-10-06 14:22:25 +0200

    Feature/office ip (#123)
    * update office ip check
    
    * update versions

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ import { checkLisaVersion } from "./lib/versions.js"
 import { generateVaultPass } from "./tasks/trellis.js"
 
 export const program = new Command()
-export const LISA_VERSION = "2.20.1"
+export const LISA_VERSION = "2.20.2"
 
 resetConf()
 checkNodeVersion()
