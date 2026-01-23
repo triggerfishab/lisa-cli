22c694ad40aa562d4ac6888ba93e45ab708bf37d
Per Lisshamre <per.lisshamre@triggerfish.se>
2026-01-23 09:42:07 +0100

    update versions

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ import { checkLisaVersion } from "./lib/versions.js"
 import { generateVaultPass } from "./tasks/trellis.js"
 
 export const program = new Command()
-export const LISA_VERSION = "2.20.2"
+export const LISA_VERSION = "2.20.3"
 
 resetConf()
 checkNodeVersion()
